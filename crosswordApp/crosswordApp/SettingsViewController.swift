1d45a4a5c07dfd6082157e58ad2db736cff63d9b
Braeden Lhamon <99349530+Blhamon055@users.noreply.github.com>
2024-04-04 13:27:26 -0500

    Worked on UI for Settings page
    Completed the UI for the settings page without working on functionality. Connections have been made but the segue has not been due to it being triggered by the title screen, divya's portion of the work

---
+++ crosswordApp/crosswordApp/SettingsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsViewController.swift
+//  crosswordApp
+//
+//  Created by Braeden Lhamon on 4/4/24.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func darkModeSwitch(_ sender: Any) {
+        //create a boolean/state where the user opens/plays the app in dark mode
+    }
+    
+    @IBAction func timeSwitch(_ sender: Any) {
+        //create a boolean which disables the timer from being measured, could also disable the time from being displayed at puzzle completion/stat page
+    }
+    
+    @IBAction func hintSwitch(_ sender: Any) {
+        //create a boolean which disables hints from coming up when a user spends a certain amount of time without inputing any characters
+    }
+    
+    @IBAction func popUpSwitch(_ sender: Any) {
+        //create a boolean which disables popups from occuring, providing feedback and/or positive reinforcement
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
