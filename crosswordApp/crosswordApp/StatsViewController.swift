847920a933587943e051ab84f453878fb8053764
Chase Durbin <chase.durbin.02@gmail.com>
2024-04-10 14:56:47 -0500

    added stats page

---
+++ crosswordApp/crosswordApp/StatsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StatsViewController.swift
+//  crosswordApp
+//
+//  Created by Chase Durbin on 4/10/24.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    @IBOutlet weak var hintsOL: UILabel!
+    @IBOutlet weak var puzzlesCompletedOL: UILabel!
+    @IBOutlet weak var timeOL: UILabel!
+    @IBOutlet weak var welcomeOL: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
