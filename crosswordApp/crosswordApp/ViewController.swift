2b26b77c00797a030be762cb68c410ab8e0c4fcc
Andrea F <143133024+aforman05@users.noreply.github.com>
2024-04-11 22:09:14 -0500

    Updates to the app from Divya
    Elements, connections, and segues for the main menu

--- crosswordApp/crosswordApp/ViewController.swift
+++ crosswordApp/crosswordApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameOL: UITextField!
+    
+ 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func easyBtn(_ sender: Any) {
+    }
+    
+    @IBAction func mediumBtn(_ sender: Any) {
+    }
+    @IBAction func hardBtn(_ sender: Any) {
+    }
 }
 
