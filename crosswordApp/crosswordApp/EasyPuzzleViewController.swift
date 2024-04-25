4bfd707abcfd7461bc01fcf1d09baef23cf1dc93
Andrea F <143133024+aforman05@users.noreply.github.com>
2024-04-25 02:22:38 -0500

    Changes to Easy View Controller
    Changes from Rusty that needed to be added separate to avoid merge conflict

--- crosswordApp/crosswordApp/EasyPuzzleViewController.swift
+++ crosswordApp/crosswordApp/EasyPuzzleViewController.swift
@@ -44,6 +44,12 @@ class EasyPuzzleViewController: UIViewController {
     
     let stats = StatsViewController()
     
+    let key=[ "Q", "U", "A", "F", "F", "T", "H", "R", "E", "E", "R", "H", "O", "D", "E", "U", "S", "E" ]
+    
+    var counter = 0
+    var timer = Timer()
+    var allCorrect = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -54,14 +60,68 @@ class EasyPuzzleViewController: UIViewController {
         if(!appDelegate.darkModeCond) {
             navigationController?.overrideUserInterfaceStyle = .light
         }
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        
+        CluesOL.text="ACROSS \n 1. Word meaning beverage that scores 20 points in Scrabble \n 6. Value of B or C in Scrabble \n 7. The R of the state abbreviation R.I. \n 8. Wear and tear \n 9. Evil spell \n DOWN \n 1. 15 minutes of an N.F.L. game. Abbr. \n 2. Sure, sure... \n 3. Came up \n 4. Company with planes that ship \n 5. Penalty for using another banks's A.T.M."
+    }
+    @objc func timerAction(){
+        counter += 1
+        TimerOL.text="\(counter)"
+        stats.time += counter
     }
     
     @IBAction func hintBtnPressed(_ sender: UIButton) {
         stats.hints += 1
+        if(A2.text != "U"){
+            A2.text="U"
+        }
+        else if(B1.text != "T"){
+            B1.text="T"
+        }
+        else if(A1.text != "Q"){
+            A1.text="Q"
+        }
+        else if(D1.text != "U"){
+            D2.text="U"
+        }
+        else if(E1.text != "H"){
+            E2.text="H"
+        }
+        else if(E3.text != "X"){
+            E3.text="X"
+        }
+        else if(C2.text != "H"){
+            C2.text="H"
+        }
+        else if(C4.text != "F"){
+            C4.text="F"
+        }
+        else {
+            self.view.makeToast("You have used too many hints.",duration: 2.0, position: .bottom)
+        }
+
     }
     
     @IBAction func checkBtnClicked(_ sender: Any) {
-        
+        let textFields = [A1, A2, A3, A4, A5, B1, B2, B3, B4, B5, C1, C2, C3, C4, C5, D1, D2, D3, E1, E2, E3]
+        let totalBoxes = textFields.count
+        for i in 1...totalBoxes-1 {
+            if(textFields[i]!.text! == key[i]){
+                allCorrect = true
+            }
+            else {
+                allCorrect = false
+                break
+            }
+        }
+        if (allCorrect == true) {
+            self.view.makeToast("You have solved the puzzle! 🤩",duration: 2.0, position: .bottom)
+            timer.invalidate()
+            stats.completed += 1
+        }
+        else {
+            self.view.makeToast("Something is wrong. 😞",duration: 2.0, position: .bottom)
+        }
     }
     
     
